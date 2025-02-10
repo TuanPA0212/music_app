87e7c959c3af6a9f2e5485bb8bc0aa9ada0230cf
Phan Tuan <phananhtuan439@gmail.com>
2025-02-11 00:32:52 +0700

    done navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/material.dart';
-
-import 'data/repository/repository.dart';
-import 'data/source/source.dart';
+import 'package:music_app/ui/home/home.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  var repository = DefaultRepository();
-  var songs = await repository.loadData();
-  if (songs != null) {
-    for (var song in songs) {
-      debugPrint(song.toString());
-    }
-  }
+  // WidgetsFlutterBinding.ensureInitialized();
+  // var repository = DefaultRepository();
+  // var songs = await repository.loadData();
+  // if (songs != null) {
+  //   for (var song in songs) {
+  //     debugPrint(song.toString());
+  //   }
+  // }
+  runApp(const MusicApp());
 }
 
-class MusicApp extends StatelessWidget {
-  const MusicApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
---
+++ lib/ui/settings/settings.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SettingsTabs extends StatelessWidget {
+  const SettingsTabs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+          child: Text("Settings Tab"),
+        )
+    );
+  }
+}
