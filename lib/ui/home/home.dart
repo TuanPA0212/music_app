9f8aafa06a27a774f8eb1bb13acaf925f8f7504f
Phan Tuan <phananhtuan439@gmail.com>
2025-02-19 00:00:44 +0700

    done song list view and bottom sheet

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:music_app/ui/discovery/discovery.dart';
+import 'package:music_app/ui/home/viewmodel.dart';
 import 'package:music_app/ui/settings/settings.dart';
 
+import '../../data/model/song.dart';
+import '../now_playing/playing.dart';
 import '../user/user.dart';
 
 class MusicApp extends StatelessWidget {
@@ -67,10 +70,160 @@ class HomeTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Tab"),
-      )
+    return const HomeTabPage();
+  }
+}
+
+class HomeTabPage extends StatefulWidget {
+  const HomeTabPage({super.key});
+
+  @override
+  State<HomeTabPage> createState() => _HomeTabPageState();
+}
+
+class _HomeTabPageState extends State<HomeTabPage> {
+  List<Song> songs = [];
+  late MusicAppViewModel _viewModel;
+
+  @override
+  void initState() {
+    _viewModel = MusicAppViewModel();
+    _viewModel.loadSongs();
+    observeData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: getBody(),
+    );
+  }
+
+  @override
+  void dispose() {
+    _viewModel.songStream.close();
+    super.dispose();
+  }
+
+  Widget getBody() {
+    bool showLoading = songs.isEmpty;
+    if (showLoading) {
+      return getProgressBar();
+    } else {
+      return getListView();
+    }
+  }
+
+  Widget getProgressBar() {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget getListView() {
+    return ListView.separated(
+      itemBuilder: (context, position) {
+        return getRow(position);
+      },
+      separatorBuilder: (context, index) {
+        return const Divider(
+          color: Colors.grey,
+          thickness: 1,
+          indent: 24,
+          endIndent: 24,
+        );
+      },
+      itemCount: songs.length,
+      // shrinkWrap: true,
+    );
+  }
+
+  Widget getRow(int index) {
+    return _SongItemSection(
+      parent: this,
+      song: songs[index],
+    );
+  }
+
+  void observeData() {
+    _viewModel.songStream.stream.listen((songList) {
+      setState(() {
+        songs.addAll(songList);
+      });
+    });
+  }
+
+  void showBottomSheet() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return ClipRRect(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+            child: Container(
+                height: 400,
+                color: Colors.grey,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      const Text("Modal Bottom Sheet"),
+                      ElevatedButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: Text("Close bottom sheet")),
+                    ],
+                  ),
+                )),
+          );
+        });
+  }
+
+  void navigate(Song song) {
+    Navigator.push(context, CupertinoPageRoute(builder: (context) {
+      return NowPlaying(
+        songs: songs,
+        playingSong: song,
+      );
+    }));
+  }
+}
+
+class _SongItemSection extends StatelessWidget {
+  const _SongItemSection({
+    required this.parent,
+    required this.song,
+  });
+
+  final _HomeTabPageState parent;
+  final Song song;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: const EdgeInsets.only(left: 24, right: 8),
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(50),
+        child: FadeInImage.assetNetwork(
+          placeholder: 'assets/itune_logo.png',
+          image: song.image,
+          width: 48,
+          height: 48,
+          imageErrorBuilder: (context, error, stackTrace) {
+            return Image.asset('assets/itune_logo.png', width: 48, height: 48);
+          },
+        ),
+      ),
+      title: Text(song.title),
+      subtitle: Text(song.artist),
+      trailing: IconButton(
+          onPressed: () {
+            parent.showBottomSheet();
+          },
+          icon: const Icon(Icons.more_horiz)),
+      onTap: () {
+        parent.navigate(song);
+      },
     );
   }
 }
