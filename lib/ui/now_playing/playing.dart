2581439ec127a3e57e9c993daf36acf7c3711e29
Phan Tuan <phananhtuan439@gmail.com>
2025-03-03 14:11:54 +0700

    add music progress bar

--- lib/ui/now_playing/playing.dart
+++ lib/ui/now_playing/playing.dart
@@ -1,7 +1,9 @@
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../data/model/song.dart';
+import 'audio_player_manager.dart';
 
 class NowPlaying extends StatelessWidget {
   const NowPlaying({super.key, required this.playingSong, required this.songs});
@@ -29,17 +31,24 @@ class NowPlayingPage extends StatefulWidget {
 class _NowPlayingPageState extends State<NowPlayingPage>
     with SingleTickerProviderStateMixin {
   late AnimationController _imageAnimContronller;
+  late AudioPlayerManager _audioPlayerManager;
 
   @override
   void initState() {
     super.initState();
     _imageAnimContronller = AnimationController(
         vsync: this, duration: const Duration(microseconds: 12000));
+    _audioPlayerManager =
+        AudioPlayerManager(songUrl: widget.playingSong.source);
+    _audioPlayerManager.init();
   }
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
+    final screenWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
     const delta = 64;
     final radius = (screenWidth - delta) / 2;
     return CupertinoPageScaffold(
@@ -63,6 +72,8 @@ class _NowPlayingPageState extends State<NowPlayingPage>
                 const SizedBox(
                   height: 48,
                 ),
+
+                // Song image
                 RotationTransition(
                   turns: Tween(begin: 0.0, end: 1.0)
                       .animate(_imageAnimContronller),
@@ -83,6 +94,8 @@ class _NowPlayingPageState extends State<NowPlayingPage>
                     ),
                   ),
                 ),
+
+                //Song name
                 Padding(
                   padding: const EdgeInsets.only(top: 64, bottom: 16),
                   child: SizedBox(
@@ -92,45 +105,81 @@ class _NowPlayingPageState extends State<NowPlayingPage>
                         IconButton(
                           onPressed: () {},
                           icon: const Icon(Icons.share_outlined),
-                          color: Theme.of(context).colorScheme.primary,
+                          color: Theme
+                              .of(context)
+                              .colorScheme
+                              .primary,
                         ),
                         Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text(
                               widget.playingSong.title,
-                              style: Theme.of(context)
+                              style: Theme
+                                  .of(context)
                                   .textTheme
                                   .bodyMedium!
                                   .copyWith(
-                                      color: Theme.of(context)
-                                          .textTheme
-                                          .bodyMedium!
-                                          .color),
+                                  color: Theme
+                                      .of(context)
+                                      .textTheme
+                                      .bodyMedium!
+                                      .color),
                             ),
                             const SizedBox(height: 8),
                             Text(widget.playingSong.artist,
-                                style: Theme.of(context)
+                                style: Theme
+                                    .of(context)
                                     .textTheme
                                     .bodyMedium!
                                     .copyWith(
-                                        color: Theme.of(context)
-                                            .textTheme
-                                            .bodyMedium!
-                                            .color)),
+                                    color: Theme
+                                        .of(context)
+                                        .textTheme
+                                        .bodyMedium!
+                                        .color)),
                           ],
                         ),
                         IconButton(
                             onPressed: () {},
                             icon: const Icon(Icons.favorite_outline),
-                            color: Theme.of(context).colorScheme.primary),
+                            color: Theme
+                                .of(context)
+                                .colorScheme
+                                .primary),
                       ],
                     ),
                   ),
                 ),
+
+                //progress bar
+                Padding(padding: EdgeInsets.only(
+                    left: 24, top: 32, right: 24, bottom: 16),
+                  child: _progressBar())
               ],
             ),
           ),
         ));
   }
+
+  StreamBuilder<DurationState> _progressBar() {
+    return StreamBuilder<DurationState>(
+        stream: _audioPlayerManager.durationState, builder: (context, snapshot) {
+      final durationState = snapshot.data;
+      final progress = durationState?.progress ?? Duration.zero;
+      final buffered = durationState?.buffered ?? Duration.zero;
+      final total = durationState?.total ?? Duration.zero;
+      return ProgressBar(
+        progress: progress,
+        buffered: buffered,
+        total: total,
+        // onSeek: (duration) {
+        //   _player.seek(duration);
+        // },
+      );
+    });
+  }
+
 }
+
+
